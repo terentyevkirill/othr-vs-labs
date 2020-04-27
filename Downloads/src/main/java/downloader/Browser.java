7e11b539b5fe1eea6f7d55e5b4320fb7aaaf5a89
terentyevkirill <kirillterentyev1999@gmail.com>
2020-04-27 18:01:53 +0200

    übungsblatt 2 aufgabe 2 done

--- Downloads/src/main/java/downloader/Browser.java
+++ Downloads/src/main/java/downloader/Browser.java
@@ -6,99 +6,86 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.*;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 
-public class Browser extends JFrame implements ActionListener{
-	
-	private int downloads;
-	private JProgressBar[] balken;
-	private JButton startButton;
+public class Browser extends JFrame implements ActionListener {
 
-	// Deklaration Ihrer Synchronisations-Hilfsklassen hier:
+    private JProgressBar[] progressBars;
+    private JButton startButton;
 
+    // Deklaration Ihrer Synchronisations-Hilfsklassen hier:
+    private final CountDownLatch start;
+    private final CyclicBarrier target;
 
+    public Browser(int downloads) {
+        super("Mein Download-Browser");
 
-
-	
-	public Browser(int downloads) {
-		super("Mein Download-Browser");
-		this.downloads = downloads;
-		
-		// Initialisierung Ihrer Synchronisations-Hilfsklassen hier:
+        // Initialisierung Ihrer Synchronisations-Hilfsklassen hier:
+        start = new CountDownLatch(1);
+        target = new CyclicBarrier(5 + 1);  //  +1 - waiting thread
 
 
+        // Aufbau der GUI-Elemente:
+        progressBars = new JProgressBar[downloads];
+        JPanel rows = new JPanel(new GridLayout(downloads, 1));
+
+        for (int i = 0; i < downloads; i++) {
+            JPanel line = new JPanel(new FlowLayout(FlowLayout.LEADING, 0, 10));
+            progressBars[i] = new JProgressBar(0, 100);
+            progressBars[i].setPreferredSize(new Dimension(500, 20));
+            line.add(progressBars[i]);
+            rows.add(line);
 
 
+            // neue Download-Threads erzeugen und starten
+            // ggf. müssen Synchronisations-Objekte im Konstruktor übergeben werden!!
+            // balken ist ebenfalls zu übergeben!
+            new Thread(new Download(progressBars[i], start, target)).start();
 
 
+        }
 
-		// Aufbau der GUI-Elemente:	
-		balken = new JProgressBar[downloads];
-		JPanel zeilen = new JPanel(new GridLayout(downloads, 1));
-		
-		for(int i=0; i<downloads; i++) {
-			JPanel reihe = new JPanel(new FlowLayout(FlowLayout.LEADING, 0, 10));
-			balken[i] = new JProgressBar(0, 100);
-                        balken[i].setPreferredSize(new Dimension(500, 20));
-			reihe.add(balken[i]);
-			zeilen.add(reihe);
+        startButton = new JButton("Downloads starten");
+        startButton.addActionListener(this);
+
+        this.add(rows, BorderLayout.CENTER);
+        this.add(startButton, BorderLayout.SOUTH);
 
 
+        pack();
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 
-			// neue Download-Threads erzeugen und starten
-			// ggf. müssen Synchronisations-Objekte im Konstruktor übergeben werden!!
-			// balken ist ebenfalls zu übergeben!
+    public static void main(String[] args) {
+        new Browser(5);
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Blockierte Threads jetzt laufen lassen:
+        start.countDown();
 
+        startButton.setEnabled(false);
+        startButton.setSelected(false);
+        startButton.setText("Downloads laufen...");
 
-		}
+        // Auf Ende aller Download-Threads warten ... erst dann die Beschriftung ändern
+        // Achtung, damit die Oberflaeche "reaktiv" bleibt dies in einem eigenen Runnable ausfuehren!
+        new Thread(() -> {
+            try {
+                target.await();
+            } catch (InterruptedException | BrokenBarrierException ex) {
+                ex.printStackTrace();
+            }
+            startButton.setText("Ende");
+        }).start();
 
-		startButton = new JButton("Downloads starten");
-		startButton.addActionListener(this);
-		
-		this.add(zeilen, BorderLayout.CENTER);
-		this.add(startButton, BorderLayout.SOUTH);
-		
-		
-		pack();
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
-	
-	public static void main(String[] args) throws InterruptedException {
-		new Browser(5);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// Blockierte Threads jetzt laufen lassen:
-
-
-
-
-
-
-		startButton.setEnabled(false);
-		startButton.setSelected(false);
-		startButton.setText("Downloads laufen...");
-		
-		// Auf Ende aller Download-Threads warten ... erst dann die Beschriftung ändern
-		// Achtung, damit die Oberflaeche "reaktiv" bleibt dies in einem eigenen Runnable ausfuehren!
-
-
-
-
-
-
-
-
-
-
-	}
+    }
 
 }
--- Downloads/src/main/java/downloader/Download.java
+++ Downloads/src/main/java/downloader/Download.java
@@ -2,17 +2,49 @@ package downloader;
 
 
 import javax.swing.JProgressBar;
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 // aktive Klasse
-public class Download
-{
-	
-	private final JProgressBar balken;
+public class Download implements Runnable {
+
+    private final JProgressBar progressBar;
     // weitere Attribute zur Synchronisation hier definieren
-    
-	public Download(JProgressBar balken /* ggf. weitere Objekte */) {
-		this.balken = balken;
-        // ...
+    private final CountDownLatch start;
+    private final CyclicBarrier target;
+
+    public Download(JProgressBar progressBar, CountDownLatch start, CyclicBarrier target) {
+        this.progressBar = progressBar;
+        this.start = start;
+        this.target = target;
+    }
+
+    @Override
+    public void run() {
+        // wait for button click
+        try {
+            start.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Random rnd = new Random();
+        for (int progress = 0; progress <= 100; progress++) {
+            progressBar.setValue(progress);
+            try {
+                long waitingTime = rnd.nextInt(300);
+                Thread.sleep(waitingTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        // wait for the slowest
+        try {
+            target.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
     }
 
 
