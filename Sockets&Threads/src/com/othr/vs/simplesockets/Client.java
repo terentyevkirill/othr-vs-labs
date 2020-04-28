2712115463243679c3bd7a57158c550126c59d05
terentyevkirill <kirillterentyev1999@gmail.com>
2020-04-28 20:26:34 +0200

    aufgabe 1 done: client sends text to server, server receives it and sends *back*

--- Sockets&Threads/src/com/othr/vs/simplesockets/Client.java
+++ Sockets&Threads/src/com/othr/vs/simplesockets/Client.java
@@ -1,4 +1,35 @@
 package com.othr.vs.simplesockets;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) {
+//        String serverHost = args[0];
+//        int serverPort = Integer.parseInt(args[1]);
+        String serverHost = "localhost";
+        int serverPort = 1200;
+
+        System.out.println("Enter text: ");
+        Scanner scanner = new Scanner(System.in);
+        String textForServer = scanner.nextLine();
+        try {
+            Socket serverSocket = new Socket(serverHost, serverPort);
+
+            OutputStream out = serverSocket.getOutputStream();
+            PrintWriter writer = new PrintWriter(out);
+
+            InputStream in = serverSocket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            writer.println(textForServer);
+            writer.flush();
+
+            String serverReply = reader.readLine();
+            System.out.println("Reply from Server: " + serverReply);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- Sockets&Threads/src/com/othr/vs/simplesockets/Server.java
+++ Sockets&Threads/src/com/othr/vs/simplesockets/Server.java
@@ -1,4 +1,34 @@
 package com.othr.vs.simplesockets;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public static void main(String[] args) {
+//        int port = Integer.parseInt(args[0]);
+        int port = 1200;
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while (true) {
+                System.out.println("Waiting for connection...");
+                Socket clientSocket = serverSocket.accept(); // blokierend
+
+                InputStream in = clientSocket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                OutputStream out = clientSocket.getOutputStream();
+                PrintWriter writer = new PrintWriter(out);
+
+                String clientRequest = reader.readLine();   // blokierend
+                System.out.println("Received: " + clientRequest);
+
+                writer.println("*" + clientRequest + "*");
+                writer.flush();
+
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
