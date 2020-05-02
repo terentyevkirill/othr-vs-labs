08df263af0668328726bb9af2d73f810988e8605
terentyevkirill <kirillterentyev1999@gmail.com>
2020-05-02 18:11:09 +0200

    REG with no args cmd added - shows registered users

--- Sockets&Threads/src/com/othr/vs/messagingservice/server/service/ClientRequest.java
+++ Sockets&Threads/src/com/othr/vs/messagingservice/server/service/ClientRequest.java
@@ -8,6 +8,7 @@ import static com.othr.vs.messagingservice.server.service.MessagingService.*;
 import java.io.*;
 import java.net.Socket;
 import java.util.List;
+import java.util.Set;
 
 // Active class
 public class ClientRequest implements Runnable {
@@ -26,9 +27,10 @@ public class ClientRequest implements Runnable {
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
             PrintWriter writer = new PrintWriter(out);
 
-            writer.println("Dieser Server verwendet folgendes Protokoll:");
-            writer.println("REGLisa");
-            writer.println("SNDLisa#Tom#Hi Thommy, how are you today?");
+            writer.println("This Server uses protocol:");
+            writer.println("REG");
+            writer.println("REGAlex");
+            writer.println("SNDAlex#Tom#Hi Tommy, how are you today?");
             writer.println("RCVTom");
             writer.flush();
 
@@ -37,37 +39,49 @@ public class ClientRequest implements Runnable {
             String paramString = request.substring(3).trim();
             String[] params = paramString.split("#");
             String user = params[0].trim();
-            switch (command) {
-                case REGISTER_CMD:
-                    if (store.addUser(user) != null) {
-                        writer.println("OK " + user);
-                    } else {
-                        writer.println("<ERROR> Could not add user. User " + user + " already exists!");
-                    }
-                    writer.flush();
-                    break;
-                case SEND_CMD:
-                    String to = params[1].trim();
-                    String text = params[2].trim();
-                    Message message = store.addMessage(user, to, text);
-                    if (message != null) {
-                        writer.println("OK " + message);
-                    } else {
-                        writer.println("<ERROR> Could not send message. Check sender and receiver usernames!");
-                    }
-                    writer.flush();
-                    break;
-                case RECEIVE_CMD:
-                    List<Message> messages = store.getNewMessages(user);
-                    if (messages != null) {
-                        writer.println("OK " + messages.size());
-                        messages.forEach(writer::println);
-                    } else {
-                        writer.println("<ERROR> Could not receive messages. User " + user + " not found!");
-                    }
-                    writer.flush();
-                    break;
+            if (user.isEmpty()) {
+                Set<String> users = store.getUsers();
+                if (users.isEmpty()) {
+                    writer.println("No registered users yet.");
+                } else {
+                    writer.println("Registered users:");
+                    users.forEach(writer::println);
+                }
+                writer.flush();
+            } else {
+                switch (command) {
+                    case REGISTER_CMD:
+                        if (store.addUser(user) != null) {
+                            writer.println("OK " + user);
+                        } else {
+                            writer.println("<ERROR> Could not add user. User " + user + " already exists!");
+                        }
+                        writer.flush();
+                        break;
+                    case SEND_CMD:
+                        String to = params[1].trim();
+                        String text = params[2].trim();
+                        Message message = store.addMessage(user, to, text);
+                        if (message != null) {
+                            writer.println("OK " + message);
+                        } else {
+                            writer.println("<ERROR> Could not send message. Check sender and receiver usernames!");
+                        }
+                        writer.flush();
+                        break;
+                    case RECEIVE_CMD:
+                        List<Message> messages = store.getNewMessages(user);
+                        if (messages != null) {
+                            writer.println("OK " + messages.size());
+                            messages.forEach(writer::println);
+                        } else {
+                            writer.println("<ERROR> Could not receive messages. User " + user + " not found!");
+                        }
+                        writer.flush();
+                        break;
+                }
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
