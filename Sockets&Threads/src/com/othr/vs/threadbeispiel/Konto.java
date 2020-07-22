40314c070d2b73ecf35bdd10f5fe29ba3d4407b5
terentyevkirill <kirillterentyev1999@gmail.com>
2020-07-22 11:57:12 +0200

    synchronisierung mithilfe des monitors

--- Sockets&Threads/src/com/othr/vs/threadbeispiel/Konto.java
+++ Sockets&Threads/src/com/othr/vs/threadbeispiel/Konto.java
@@ -2,16 +2,31 @@ package com.othr.vs.threadbeispiel;
 
 public class Konto {
     private int kontostand;
+    private final Object monitor = new Object();
 
-    public void einzahlen(int beitrag) {
-        this.kontostand = this.kontostand + beitrag;
+    public void einzahlen(int betrag) {
+        synchronized (monitor) {
+            this.kontostand = this.kontostand + betrag;
+        }
     }
 
-    public void auszahlen(int beitrag) {
-        this.kontostand = this.kontostand - beitrag;
+    public void auszahlen(int betrag) {
+        synchronized (monitor) {
+            while (this.kontostand < betrag) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    // ignore
+                }
+            }
+            this.kontostand = this.kontostand - betrag;
+            monitor.notifyAll();
+        }
     }
 
     public int getKontostand() {
-        return kontostand;
+        synchronized (monitor) {
+            return kontostand;
+        }
     }
 }
